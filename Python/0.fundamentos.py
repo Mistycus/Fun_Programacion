5704e89d6e559a5c9f722a8108716f5d02040f86
Elvis <estreambmx@gmail.com>
2026-08-29 20:02:48 -0500

    sintaxis , entrada , salida ,comentarios en Python

--- Python/0.fundamentos.py
+++ Python/0.fundamentos.py
@@ -1,1 +1,31 @@
 # Fundamentos del Lenguaje Python 
+
+# Numero Entero
+edad = 27
+
+# Cadena de texto
+nombre = "Elvis"
+
+# numero decimal
+precio = 19.99
+
+# Valor logico
+estado = False
+
+# Valor Caracter
+genero = 'M'
+
+# Entrada de Datos 
+nombre = input("Ingrese su Nombre: ")
+
+# Salida de Datos
+print(f'hola,{nombre}')
+
+# Comentar 1 linea 
+'''
+Comentario en Bloque
+
+'''
+
+# Ejecutar Programa  Python
+python fundamentos.py
